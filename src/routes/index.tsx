debd01d0472e53de5bdee80088e335bdfe220cad
Felix Larsen <16463534+i4guar@users.noreply.github.com>
2022-11-27 13:55:22 +0100

    Add styling with tailwind

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,18 @@ import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
   return (
-    <>
-      <div>
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-blue-800 to-blue-900 text-white" >
+      <div className='text-5xl'>
         Hello Weather! What's up?
       </div>
-      <div>
-        -
+      <div className='text-5xl'>
+        <br/>
+        -<br/>
+        <br/>
       </div>
-      <div>
+      <div className='text-4xl'>
         At 13:33:47 it's 10° and cloudy in Hamburg.
       </div>
-    </>
+    </div>
   );
 });
