10a7e2d39d11819695a4007517180f3e2cc64dcf
Felix Larsen <16463534+i4guar@users.noreply.github.com>
2022-11-27 13:31:32 +0100

    Remove example code

--- src/root.tsx
+++ src/root.tsx
@@ -1,6 +1,5 @@
 import { component$, useStyles$ } from '@builder.io/qwik';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
-import { RouterHead } from './components/router-head/router-head';
 
 import globalStyles from './global.css?inline';
 
@@ -18,7 +17,6 @@ export default component$(() => {
       <head>
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
-        <RouterHead />
       </head>
       <body lang="en">
         <RouterOutlet />
