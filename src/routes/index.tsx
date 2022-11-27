ccd9b35207c05e79a755619b1c08db38c0f97b9a
Felix Larsen <16463534+i4guar@users.noreply.github.com>
2022-11-27 13:35:41 +0100

    Add template for target app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,16 @@ import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
   return (
-    <div>
-      Hello Weather! What's up?
-    </div>
+    <>
+      <div>
+        Hello Weather! What's up?
+      </div>
+      <div>
+        -
+      </div>
+      <div>
+        At 13:33:47 it's 10° and cloudy in Hamburg.
+      </div>
+    </>
   );
 });
