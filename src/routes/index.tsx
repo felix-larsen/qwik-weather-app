856dc0d4a506207934332a99060a62c70f375a4d
Felix Larsen <16463534+i4guar@users.noreply.github.com>
2022-12-04 13:40:26 +0100

    Add working clock

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,24 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useClientEffect$, useStore } from '@builder.io/qwik';
+
+interface ClockStore {
+  hour: number;
+  minute: number;
+  second: number;
+}
 
 export default component$(() => {
+  const clockStore = useStore<ClockStore>({
+    hour: 0,
+    minute: 0,
+    second: 0,
+  });
+  
+  useClientEffect$(({track}) => {
+    updateClock(clockStore);
+    track(clockStore);
+    const timerId = setTimeout(() => updateClock(clockStore), 1000);
+    return () => clearTimeout(timerId);
+  });
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-blue-800 to-blue-900 text-white" >
       <div className='text-5xl'>
@@ -12,8 +30,15 @@ export default component$(() => {
         <br/>
       </div>
       <div className='text-4xl'>
-        At 13:33:47 it's 10° and cloudy in Hamburg.
+        At {clockStore.hour}:{clockStore.minute<10 ? "0" : "" }{clockStore.minute}:{clockStore.second<10 ? "0" : "" }{clockStore.second} it's 10° and cloudy in Hamburg.
       </div>
     </div>
   );
 });
+
+export function updateClock(store: ClockStore) {
+  const now = new Date();
+  store.second = now.getSeconds();
+  store.minute = now.getMinutes();
+  store.hour = now.getHours();
+}
